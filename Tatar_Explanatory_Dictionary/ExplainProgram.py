fd7dc5bac7c1876531491353bdf19c4f1728631f
SugarSingularity <79350028+SugarSingularity@users.noreply.github.com>
2023-12-11 22:23:36 +0300

    Работа над добавлением новых слов

--- Tatar_Explanatory_Dictionary/ExplainProgram.py
+++ Tatar_Explanatory_Dictionary/ExplainProgram.py
@@ -1,3 +1,73 @@
+def file_Txt(Your_word) -> str:
+    """
+    Находит подходящий файл по слову
+    :return: подходящий файл
+    """
+    match Your_word[0]:
+        case "А":
+            return "Aa.txt"
+        case "Ә":
+            return "A`a`.txt"
+        case "Б":
+            return "Bb.txt"
+        case "В":
+            return "Vv.txt"
+        case "Г":
+            return "Gg.txt"
+        case "Д":
+            return "Dd.txt"
+        case "Җ" | "Ж":
+            return "Zhzh.txt"
+        case "З":
+            return "Zz.txt"
+        case "И":
+            return "Ii.txt"
+        case "Й":
+            return "Jj.txt"
+        case "К":
+            return "Kk.txt"
+        case "Л":
+            return "Ll.txt"
+        case "М":
+            return "Mm.txt"
+        case "Н":
+            return "Nn.txt"
+        case "О":
+            return "Oo.txt"
+        case "Ө":
+            return "O`o`.txt"
+        case "П":
+            return "Pp.txt"
+        case "Р":
+            return "Rr.txt"
+        case "С":
+            return "Ss.txt"
+        case "Т":
+            return "Tt.txt"
+        case "У":
+            return "Uu.txt"
+        case "Ү":
+            return "U`u`.txt"
+        case "Ф":
+            return "Ff.txt"
+        case "Х":
+            return "Hh.txt"
+        case "Һ":
+            return "H`h`.txt"
+        case "Ч":
+            return "Chch.txt"
+        case "Ш":
+            return "Shsh.txt"
+        case "Ы":
+            return "Yy.txt"
+        case "Э":
+            return "Ee.txt"
+        case "Ю":
+            return "Juju.txt"
+        case "Я":
+            return "Jaja.txt"
+
+
 def Explain(Your_word: str) -> str:
     """
     Функция находит слова(param word) и даёт ему объяснение, найдя его в тестовом файле
@@ -5,79 +75,10 @@ def Explain(Your_word: str) -> str:
     :return: текст объяснения
     """
 
-    def file_Txt() -> str:
-        """
-        Находит подходящий файл по слову
-        :return: подходящий файл
-        """
-        match Your_word[0]:
-            case "А":
-                return "Aa.txt"
-            case "Ә":
-                return "A`a`.txt"
-            case "Б":
-                return "Bb.txt"
-            case "В":
-                return "Vv.txt"
-            case "Г":
-                return "Gg.txt"
-            case "Д":
-                return "Dd.txt"
-            case "Җ" | "Ж":
-                return "Zhzh.txt"
-            case "З":
-                return "Zz.txt"
-            case "И":
-                return "Ii.txt"
-            case "Й":
-                return "Jj.txt"
-            case "К":
-                return "Kk.txt"
-            case "Л":
-                return "Ll.txt"
-            case "М":
-                return "Mm.txt"
-            case "Н":
-                return "Nn.txt"
-            case "О":
-                return "Oo.txt"
-            case "Ө":
-                return "O`o`.txt"
-            case "П":
-                return "Pp.txt"
-            case "Р":
-                return "Rr.txt"
-            case "С":
-                return "Ss.txt"
-            case "Т":
-                return "Tt.txt"
-            case "У":
-                return "Uu.txt"
-            case "Ү":
-                return "U`u`.txt"
-            case "Ф":
-                return "Ff.txt"
-            case "Х":
-                return "Hh.txt"
-            case "Һ":
-                return "H`h`.txt"
-            case "Ч":
-                return "Chch.txt"
-            case "Ш":
-                return "Shsh.txt"
-            case "Ы":
-                return "Yy.txt"
-            case "Э":
-                return "Ee.txt"
-            case "Ю":
-                return "Juju.txt"
-            case "Я":
-                return "Jaja.txt"
-
     wordEX = Your_word + " -"
 
     ReturnText = ""
-    fileo = open(file_Txt(), "r", encoding="utf-8")
+    fileo = open(file_Txt(Your_word), "r", encoding="utf-8")
     fileText = fileo.read()
     if word in fileText:
         index = fileText.find(wordEX)
@@ -94,6 +95,15 @@ def Explain(Your_word: str) -> str:
         return ReturnText
     else:
         return "Слово не найдено"
+
+
+def WordAdd(New_word: str):
+    """
+    Функция для добавления слов в словарь
+    :param New_word: Новое слово
+    :return: Записывает новое слово для дальнейшего использования
+    """
+    pass
 
 
 try:
