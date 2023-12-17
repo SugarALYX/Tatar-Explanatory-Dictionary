c8811a9f231af6109f63b7edeb2753e7ac2b08ed
SugarSingularity <79350028+SugarSingularity@users.noreply.github.com>
2023-12-17 14:25:34 +0300

    Добавил функцию поиска случайного слова и случайного объяснения для него

--- gui/WordFind.py
+++ gui/WordFind.py
@@ -1,7 +1,22 @@
 import random as r
 
 
-def file_txt(Your_word: str, pathx: str) -> str:
+def random_file(way: str) -> str:
+    """
+    Функция выбирает случайный файл из словаря
+    :param way: путь к словарю
+    :return: текст словаря(одной буквы)
+    """
+    files = ["Aa.txt", "A`a`.txt", "Bb.txt", "Vv.txt", "Gg.txt", "Dd.txt", "Zhzh.txt", "Zhzh.txt", "Zz.txt", "Ii.txt",
+             "Jj.txt", "Kk.txt", "Ll.txt", "Mm.txt", "Nn.txt", "Oo.txt", "O`o`.txt", "Pp.txt", "Ss.txt", "Tt.txt",
+             "Uu.txt", "U`u`.txt", "Ff.txt", "Hh.txt", "H`h`.txt", "Chch.txt", "Shsh.txt", "Yy.txt", "Ee.txt",
+             "Juju.txt", "Jaja.txt"]
+    fileo = open(f"{way}{files[r.randint(0, len(files)) - 1]}", "r", encoding="utf-8")
+    fileText = fileo.read()
+    return fileText
+
+
+def path(Your_word: str, way: str) -> str:
     """
     Находит подходящий файл по слову
     :return: подходящий файл
@@ -40,13 +55,13 @@ def file_txt(Your_word: str, pathx: str) -> str:
         "Ю": "Juju.txt",
         "Я": "Jaja.txt"
     }
-    return pathx + Files[Your_word[0]]
+    return way + Files[Your_word[0]]
 
 
-def Explain(Your_word: str, path: str) -> str:
+def Explain(Your_word: str, way: str) -> str:
     """
     Функция находит слова(param word) и даёт ему объяснение, найдя его в тестовом файле
-    :param path: путь к файлу
+    :param way: путь к словарю
     :param Your_word: Искомое слово
     :return: текст объяснения
     """
@@ -55,7 +70,7 @@ def Explain(Your_word: str, path: str) -> str:
         wordEX = Your_word + " -"
 
         ReturnText = ""
-        fileo = open(file_txt(Your_word, path), "r", encoding="utf-8")
+        fileo = open(path(Your_word, way), "r", encoding="utf-8")
         fileText = fileo.read()
         if wordEX in fileText:
             index = fileText.find(wordEX)
@@ -78,18 +93,13 @@ def WordAdd(New_word: str):
     pass
 
 
-def random_word(path: str) -> str:
+def random_word_with_explain(way: str) -> str:
     """
     Функция находит случайное слово и её определение
-    :param path: путь к репозиторию
+    :param way: путь к репозиторию
     :return:
     """
-    files = ["Aa.txt", "A`a`.txt", "Bb.txt", "Vv.txt", "Gg.txt", "Dd.txt", "Zhzh.txt", "Zhzh.txt", "Zz.txt", "Ii.txt",
-             "Jj.txt", "Kk.txt", "Ll.txt", "Mm.txt", "Nn.txt", "Oo.txt", "O`o`.txt", "Pp.txt", "Ss.txt", "Tt.txt",
-             "Uu.txt", "U`u`.txt", "Ff.txt", "Hh.txt", "H`h`.txt", "Chch.txt", "Shsh.txt", "Yy.txt", "Ee.txt",
-             "Juju.txt", "Jaja.txt"]
-    fileo = open(f"{path}{files[r.randint(0, len(files))]}", "r", encoding="utf-8")
-    fileText = fileo.read()
+    fileText = random_file(way)
     ReturnWord = ""
     index = r.randint(0, len(fileText))
     while True:
@@ -106,3 +116,40 @@ def random_word(path: str) -> str:
         except IndexError:
             index = r.randint(0, len(fileText))
         return ReturnWord
+
+
+def random_word_random_explain(way: str) -> str:
+    ReturnWord = ""
+    ForWordWay = random_file(way)
+    index = r.randint(0, len(ForWordWay))
+    while True:
+        try:
+            while ForWordWay[index] != "\n":
+                index += 1
+            while ForWordWay[index] == "\n":
+                index += 1
+            while ForWordWay[index] != "-":
+                ReturnWord += ForWordWay[index]
+                index += 1
+            if ForWordWay[index] == "-":
+                break
+        except IndexError:
+            index = r.randint(0, len(ForWordWay))
+    ForWordExplainWay = random_file(way)
+    index = r.randint(0, len(ForWordExplainWay))
+    while True:
+        try:
+            while ForWordExplainWay[index] != "\n":
+                index += 1
+            while ForWordExplainWay[index] == "\n":
+                index += 1
+            while ForWordExplainWay[index] != " ":
+                index += 1
+            while ForWordExplainWay[index] != "\n":
+                ReturnWord += ForWordExplainWay[index]
+                index += 1
+            if ForWordExplainWay[index] == "\n":
+                break
+        except IndexError:
+            index = r.randint(0, len(ForWordExplainWay))
+    return ReturnWord
