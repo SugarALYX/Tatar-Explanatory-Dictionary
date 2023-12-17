1de179a6d8496e5fdf91a175d84f105f19496e46
SugarSingularity <79350028+SugarSingularity@users.noreply.github.com>
2023-12-17 17:41:32 +0300

    маленькие исправления

--- gui/MENU.py
+++ gui/MENU.py
@@ -1,131 +1,143 @@
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+from Simplemod import SimpleModMenu
 
-ASSETS_PATH = r"assets/frame0"
 
-window = Tk()
-window.title("Татарский толковый словарь")
-window.geometry("1280x720")
-window.configure(bg="#504D4D")
+class MenuOfDictionary:
+    def __init__(self):
+        ASSETS_PATH = r"assets/frame0"
+        self.Menu = Tk()
+        self.Menu.title("Татарский толковый словарь")
+        self.Menu.geometry("1280x720")
+        self.Menu.configure(bg="#504D4D")
+        self.Menu.resizable(False, False)
 
-canvas = Canvas(
-    window,
-    bg="#504D4D",
-    height=720,
-    width=1280,
-    bd=0,
-    highlightthickness=0,
-    relief="ridge"
-)
+        def CreatSimpleModWindow():
+            SimpleModMenu(self.Menu)
 
-canvas.place(x=0, y=0)
-image_image_1 = PhotoImage(
-    file=f"{ASSETS_PATH}/image_1.png")
-image_1 = canvas.create_image(
-    665.0,
-    59.0,
-    image=image_image_1
-)
+        self.canvas = Canvas(
+            self.Menu,
+            bg="#504D4D",
+            height=720,
+            width=1280,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge"
+        )
 
-canvas.create_text(
-    295.0,
-    29.0,
-    anchor="nw",
-    text="  Татарский толковый словарь",
-    fill="#010101",
-    font=("Inter SemiBold", 50 * -1)
-)
+        self.canvas.place(x=0, y=0)
+        self.image_image_1 = PhotoImage(
+            file=f"{ASSETS_PATH}/image_1.png")
+        self.image_1 = self.canvas.create_image(
+            665.0,
+            59.0,
+            image=self.image_image_1
+        )
 
-button_image_1 = PhotoImage(
-    file=f"{ASSETS_PATH}/button_1.png")
-button_1 = Button(
-    image=button_image_1,
-    borderwidth=0,
-    highlightthickness=0,
-    command=lambda: print("button_1 clicked"),
-    relief="flat"
-)
-button_1.place(
-    x=46.0,
-    y=185.0,
-    width=524.0,
-    height=105.0
-)
+        self.canvas.create_text(
+            295.0,
+            29.0,
+            anchor="nw",
+            text="  Татарский толковый словарь",
+            fill="#010101",
+            font=("Inter SemiBold", 50 * -1)
+        )
 
-button_image_2 = PhotoImage(
-    file=f"{ASSETS_PATH}/button_2.png")
-button_2 = Button(
-    image=button_image_2,
-    borderwidth=0,
-    highlightthickness=0,
-    command=lambda: print("button_2 clicked"),
-    relief="flat"
-)
-button_2.place(
-    x=46.0,
-    y=541.0,
-    width=524.0,
-    height=105.0
-)
+        self.button_image_1 = PhotoImage(
+            file=f"{ASSETS_PATH}/button_1.png")
+        self.MenuSimpleModButton = Button(
+            image=self.button_image_1,
+            borderwidth=0,
+            highlightthickness=0,
+            command=CreatSimpleModWindow,
+            relief="flat"
+        )
+        self.MenuSimpleModButton.place(
+            x=46.0,
+            y=185.0,
+            width=524.0,
+            height=105.0
+        )
 
-button_image_3 = PhotoImage(
-    file=f"{ASSETS_PATH}/button_3.png")
-button_3 = Button(
-    image=button_image_3,
-    borderwidth=0,
-    highlightthickness=0,
-    command=lambda: print("button_3 clicked"),
-    relief="flat"
-)
-button_3.place(
-    x=46.0,
-    y=355.0,
-    width=524.0,
-    height=105.0
-)
+        self.button_image_2 = PhotoImage(
+            file=f"{ASSETS_PATH}/button_2.png")
+        self.GameModButton = Button(
+            image=self.button_image_2,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_2 clicked"),
+            relief="flat"
+        )
+        self.GameModButton.place(
+            x=46.0,
+            y=541.0,
+            width=524.0,
+            height=105.0
+        )
 
-image_image_2 = PhotoImage(
-    file=f"{ASSETS_PATH}/image_2.png")
-image_2 = canvas.create_image(
-    939.0,
-    415.0,
-    image=image_image_2
-)
+        self.button_image_3 = PhotoImage(
+            file=f"{ASSETS_PATH}/button_3.png")
+        self.RandomModButton = Button(
+            image=self.button_image_3,
+            borderwidth=0,
+            highlightthickness=0,
+            relief="flat"
+        )
+        self.RandomModButton.place(
+            x=46.0,
+            y=371.0,
+            width=524.0,
+            height=105.0
+        )
 
-canvas.create_text(
-    655.0,
-    576.0,
-    anchor="nw",
-    text="""Приложение сделал ученик 10 класса
+        self.image_image_2 = PhotoImage(
+            file=f"{ASSETS_PATH}/image_2.png")
+        self.image_2 = self.canvas.create_image(
+            939.0,
+            415.0,
+            image=self.image_image_2
+        )
+
+        self.canvas.create_text(
+            655.0,
+            576.0,
+            anchor="nw",
+            text="""Приложение сделал ученик 10 класса
 МБОУ “Нурминская СОШ”,Шайхутдинов Абу Бакр Ильгамович""",
-    fill="#171717",
-    font=("Inter SemiBold", 20 * -1)
-)
+            fill="#171717",
+            font=("Inter SemiBold", 20 * -1)
+        )
 
-canvas.create_text(
-    655.0,
-    267.0,
-    anchor="nw",
-    text="Простой режим: вы вводите слово, программа \nнаходит этому слову определение\n\nСлучайный режим: вы "
-         "нажимаете на кнопку и\nполучаете случайное слово с определением\n\nИгра: вам даётся случайное слово и "
-         "случайное\nопределение, вы должны ответить правильно\nли подобрано определение",
-    fill="#0B0B0B",
-    font=("Inter SemiBold", 20 * -1)
-)
+        self.canvas.create_text(
+            655.0,
+            267.0,
+            anchor="nw",
+            text="Простой режим: вы вводите слово, программа \nнаходит этому слову определение\n\nСлучайный режим: вы "
+                 "нажимаете на кнопку и\nполучаете случайное слово с определением\n\nИгра: вам даётся случайное слово и"
+                 "случайное\nопределение, вы должны ответить правильно\nли подобрано определение",
+            fill="#0B0B0B",
+            font=("Inter SemiBold", 20 * -1)
+        )
 
-image_image_3 = PhotoImage(
-    file=f"{ASSETS_PATH}/image_3.png")
-image_3 = canvas.create_image(
-    145.0,
-    80.0,
-    image=image_image_3
-)
+        self.image_image_3 = PhotoImage(
+            file=f"{ASSETS_PATH}/image_3.png")
+        self.image_3 = self.canvas.create_image(
+            145.0,
+            80.0,
+            image=self.image_image_3
+        )
 
-image_image_4 = PhotoImage(
-    file=f"{ASSETS_PATH}/image_4.png")
-image_4 = canvas.create_image(
-    1176.0,
-    86.0,
-    image=image_image_4
-)
-window.resizable(False, False)
-window.mainloop()
+        self.image_image_4 = PhotoImage(
+            file=f"{ASSETS_PATH}/image_4.png")
+        self.image_4 = self.canvas.create_image(
+            1176.0,
+            86.0,
+            image=self.image_image_4
+        )
+
+    def run(self):
+        self.Menu.mainloop()
+
+
+if __name__ == "__main__":
+    app = MenuOfDictionary()
+    app.run()
---
+++ gui/Simplemod.py
@@ -0,0 +1,98 @@
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, END, Toplevel
+from gui.WordFind import Explain
+
+
+class SimpleModMenu:
+    def __init__(self, parent):
+        ASSETS_PATHz = r"assets/frame1"
+        self.SimpleModWindow = Toplevel(parent)
+        self.SimpleModWindow.geometry("1280x720")
+        self.SimpleModWindow.configure(bg="#514D4D")
+        self.SimpleModWindow.title("Татарский толковый словарь")
+        self.SimpleModWindow.resizable(False, False)
+
+        self.canv = Canvas(
+            self.SimpleModWindow,
+            bg="#514D4D",
+            height=720,
+            width=1280,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge"
+        )
+
+        self.canv.place(x=0, y=0)
+        self.entry_image_1 = PhotoImage(
+            file=f"{ASSETS_PATHz}/entry_1.png")
+        self.entry_bg_1 = self.canv.create_image(
+            642.0,
+            457.5,
+            image=self.entry_image_1
+        )
+        self.Explaining = Text(
+            self.SimpleModWindow,
+            font=("Inter SemiBold", 25 * -1),
+            bd=0,
+            bg="#6EB755",
+            fg="#000716",
+            highlightthickness=0,
+            wrap="word",
+        )
+        self.Explaining.place(
+            x=46.0,
+            y=222.0,
+            width=1192.0,
+            height=469.0
+        )
+
+        self.canv.create_rectangle(
+            26.0,
+            37.0,
+            667.0,
+            189.0,
+            fill="#6EB755",
+            outline="")
+
+        self.entry_image_2 = PhotoImage(
+            file=f"{ASSETS_PATHz}/entry_2.png",)
+        self.entry_bg_2 = self.canv.create_image(
+            346.0,
+            113.5,
+            image=self.entry_image_2
+        )
+        self.YourWord = Entry(
+            self.SimpleModWindow,
+            font=("Inter SemiBold", 50 * -1),
+            bd=0,
+            bg="#6EB755",
+            fg="#000716",
+            highlightthickness=0
+        )
+        self.YourWord.place(
+            x=44.0,
+            y=45.0,
+            width=604.0,
+            height=135.0
+        )
+
+        self.button_image_1 = PhotoImage(
+            file=f"{ASSETS_PATHz}/button_1.png")
+
+        def Find():
+            self.Explaining.delete(1.0, END)
+            self.Explaining.insert(1.0, Explain(self.YourWord.get(), r"Words/"))
+
+        self.FindButton = Button(
+            self.SimpleModWindow,
+            image=self.button_image_1,
+            borderwidth=0,
+            highlightthickness=0,
+            command=Find,
+            relief="flat"
+        )
+        self.FindButton.place(
+            x=706.0,
+            y=37.0,
+            width=551.6851196289062,
+            height=152.0
+        )
